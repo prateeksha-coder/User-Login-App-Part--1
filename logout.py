69c4c718f083e4b478d8d6ad11570e612eb03108
prateeksha-coder <prateeksha.codingal@gmail.com>
2026-05-19 15:56:41 +0530

    commit

---
+++ logout.py
@@ -0,0 +1,8 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route('/logout')
+def logout():
+    name = ''
+    id = ''
+    msg = 'Logged out succesfully'
+    return render_template('login.html',msg=msg, name=name, id=id)
